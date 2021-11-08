a61d404476fc9b115cd5d66bc9b2459792653bb4
Navendu Pottekkat <navendupottekkat@gmail.com>
2021-11-09 00:08:24 +0530

    add istio deploy script
    Signed-off-by: Navendu Pottekkat <navendupottekkat@gmail.com>

---
+++ .github/workflows/scripts/istio_deploy.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+
+curl -L https://istio.io/downloadIstio | ISTIO_VERSION=1.11.4 sh -
+cd istio-1.11.4
+export PATH=$PWD/bin:$PATH
+istioctl install --set profile=demo -y
+kubectl label namespace default istio-injection=enabled
+kubectl apply -f samples/bookinfo/platform/kube/bookinfo.yaml
+kubectl apply -f samples/bookinfo/networking/bookinfo-gateway.yaml
+sleep 100
+export INGRESS_PORT=$(kubectl -n istio-system get service istio-ingressgateway -o jsonpath='{.spec.ports[?(@.name=="http2")].nodePort}')
+export SECURE_INGRESS_PORT=$(kubectl -n istio-system get service istio-ingressgateway -o jsonpath='{.spec.ports[?(@.name=="https")].nodePort}')
+export INGRESS_HOST=$(minikube ip)
+export GATEWAY_URL=$INGRESS_HOST:$INGRESS_PORT
+minikube tunnel
+echo "$GATEWAY_URL"
