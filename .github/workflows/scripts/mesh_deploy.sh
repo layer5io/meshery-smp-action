506b8b214111179590670da34dd7bd08a58ffbd2
Jubril Oyetunji <cyberdev01@protonmail.com>
2021-12-09 20:14:31 +0100

    fix bug with default path
    Signed-off-by: Jubril Oyetunji <cyberdev01@protonmail.com>

--- .github/workflows/scripts/mesh_deploy.sh
+++ .github/workflows/scripts/mesh_deploy.sh
@@ -1,23 +1,15 @@
 MESH=$1
-
+MESHERYCTL="/usr/local/bin/mesheryctl"
 echo "Checking if mesheryctl is installed"
-	
-if mesheryctl
-	
-then
-	echo "Found mesheryctl, deploying $MESH"
+
+
+if [ -f "${MESHERYCTL}" ]; then
+    echo "Found mesheryctl, deploy $MESH"
 	mesheryctl mesh deploy $MESH
-	
-else
-	printf "Mesheryctl not found. \nInstalling...\n"
-	curl -L https://git.io/meshery | DEPLOY_MESHERY=false bash -
-	echo "Installed mesheryctl successfully!"
-	mesheryctl mesh deploy $MESH
-	
+   else
+      printf "Mesheryctl not found. \nInstalling...\n"
+	  curl -L https://git.io/meshery | DEPLOY_MESHERY=false bash -
+	  echo "Installed mesheryctl successfully!"
+	  mesheryctl mesh deploy $MESH
+     
 fi
-
-
-
-
-
-
