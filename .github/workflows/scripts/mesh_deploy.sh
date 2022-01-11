066eb6190708e116c178ffb5cd8894f8f06781f0
Jubril Oyetunji <cyberdev01@protonmail.com>
2022-01-11 14:23:45 +0100

    fix format issues
    Signed-off-by: Jubril Oyetunji <cyberdev01@protonmail.com>

--- .github/workflows/scripts/mesh_deploy.sh
+++ .github/workflows/scripts/mesh_deploy.sh
@@ -2,14 +2,12 @@ MESH=$1
 MESHERYCTL="/usr/local/bin/mesheryctl"
 echo "Checking if mesheryctl is installed"
 
-
-if [ -f "${MESHERYCTL}" ]; then
-    echo "Found mesheryctl, deploy $MESH"
+if [  -f $MESHERYCTL ]; then
+        echo "Found mesheryctl, deploy $MESH"
 	mesheryctl mesh deploy $MESH
-   else
-      printf "Mesheryctl not found. \nInstalling...\n"
-	  curl -L https://git.io/meshery | DEPLOY_MESHERY=false bash -
-	  echo "Installed mesheryctl successfully!"
-	  mesheryctl mesh deploy $MESH
-     
-fi
+    else
+        printf "Mesheryctl not found. \nInstalling...\n"
+	curl -L https://git.io/meshery | DEPLOY_MESHERY=false bash -
+	echo "Installed mesheryctl successfully!"
+	mesheryctl mesh deploy $MESH
+    fi
