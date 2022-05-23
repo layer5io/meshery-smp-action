506093390c3e8f99f0cfdee331b5b958854cc0b8
Aadhitya A <aadhitya864@gmail.com>
2022-05-23 14:05:04 +0530

    Change flag
    Signed-off-by: Aadhitya A <aadhitya864@gmail.com>

--- .github/workflows/scripts/istio_deploy.sh
+++ .github/workflows/scripts/istio_deploy.sh
@@ -15,7 +15,7 @@ fi
 
 sleep 10
 #mesheryctl system login --provider None
-mesheryctl mesh deploy adapter meshery-istio:10000 --token "./.github/workflows/auth.json"
+mesheryctl mesh deploy --adapter meshery-istio:10000 --token "./.github/workflows/auth.json"
 echo "Onboarding application... Standby for few minutes..."
 mesheryctl app onboard -f "https://raw.githubusercontent.com/istio/istio/blob/master/samples/bookinfo/platform/kube/bookinfo.yaml" --token "./.github/workflows/auth.json"
 
