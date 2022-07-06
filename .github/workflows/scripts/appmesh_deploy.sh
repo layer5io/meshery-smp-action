93d93db1c94c3e087b675c8df43dc86a4ad55f35
Huang Xin <xin1.huang@intel.com>
2022-07-06 10:00:06 +0800

    Fix app mesh typos.
    Signed-off-by: Huang Xin <xin1.huang@intel.com>

--- .github/workflows/scripts/appmesh_deploy.sh
+++ .github/workflows/scripts/appmesh_deploy.sh
@@ -10,12 +10,12 @@ export SERVICE_MESH='APP_MESH'
 # Check if mesheryctl is present, else install it and deploy Kuma adapter
 if ! [ -x "$(command -v mesheryctl)" ]; then
     echo 'mesheryctl is not installed. Installing mesheryctl client... Standby... (Starting Meshery as well...)' >&2
-    curl -L https://meshery.io/install | ADAPTERS=appmesh PLATFORM=kubernetes bash -
+    curl -L https://meshery.io/install | ADAPTERS=app-mesh PLATFORM=kubernetes bash -
 fi
 
 sleep 10
 
-echo 'E' | mesheryctl mesh deploy adapter meshery-appmesh:10000 --token "./.github/workflows/auth.json"
+echo 'E' | mesheryctl mesh deploy adapter meshery-app-mesh:10000 --token "./.github/workflows/auth.json"
 sleep 50
 echo "Onboarding application... Standby for few minutes..."
 mesheryctl pattern apply -f "https://raw.githubusercontent.com/service-mesh-patterns/service-mesh-patterns/master/samples/bookInfoPattern.yaml" --token "./.github/workflows/auth.json"
